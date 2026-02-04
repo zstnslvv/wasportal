36a672e7874a308b048ad041864cfe621790a02a
zstnslvv <shirnyakov@gmail.com>
2026-02-04 22:53:07 +0300

    Add oniguruma dev package for mbstring build

---
+++ backend/src/Response.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class Response
+{
+    public static function json(array $data, int $statusCode = 200): void
+    {
+        http_response_code($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        exit;
+    }
+}
